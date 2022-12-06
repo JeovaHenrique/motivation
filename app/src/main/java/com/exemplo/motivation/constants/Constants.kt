61501a232dea3920e5814f6b932e180974d25c6c
Jeova Henrique <jeovahenrique92@gmail.com>
2022-12-06 16:03:00 -0300

    filter mensage

--- app/src/main/java/com/exemplo/motivation/constants/Constants.kt
+++ app/src/main/java/com/exemplo/motivation/constants/Constants.kt
@@ -5,4 +5,10 @@ class Constants {
     object KEY {
         const val PERSON_NAME = "USER_NAME"
     }
+
+    object FILTER {
+        const val ALL = 1
+        const val HAPPY = 2
+        const val SUNNY = 3
+    }
 }
--- app/src/main/java/com/exemplo/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/exemplo/motivation/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.exemplo.motivation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.exemplo.motivation.R
 import com.exemplo.motivation.constants.Constants
 import com.exemplo.motivation.databinding.ActivityMainBinding
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mBinding: ActivityMainBinding
     private lateinit var securityPreference: SecurityPreference
+    private var category = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,18 +25,43 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         handleUserName()
 
         mBinding.buttonNewPhrase.setOnClickListener(this)
+        mBinding.imageAll.setOnClickListener(this)
+        mBinding.imageHappy.setOnClickListener(this)
+        mBinding.imageSunny.setOnClickListener(this)
 
         securityPreference = SecurityPreference(this)
     }
 
     override fun onClick(v: View) {
-        if (v.id == R.id.button_new_phrase) {
-
+        when (v.id) {
+            R.id.image_all, R.id.image_happy, R.id.image_sunny -> handleFilter(v.id)
         }
     }
 
     private fun handleUserName() {
         val name = SecurityPreference(this).getStoredString(Constants.KEY.PERSON_NAME)
         mBinding.textUserName.text = "Olá ${name}"
+    }
+
+    private fun handleFilter(id: Int) {
+
+        mBinding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.black))
+        mBinding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.black))
+        mBinding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.black))
+
+        when (id) {
+            R.id.image_all -> {
+                mBinding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                category = Constants.FILTER.ALL
+            }
+            R.id.image_happy -> {
+                mBinding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                category = Constants.FILTER.HAPPY
+            }
+            R.id.image_sunny -> {
+                mBinding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                category = Constants.FILTER.SUNNY
+            }
+        }
     }
 }
